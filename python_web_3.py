6c7695473a8b5bcff82204b834abc14e4346de61
Vishnuram <36101678+vishnuram1999@users.noreply.github.com>
2021-02-16 20:37:47 +0530

    Customer side design and functions

--- python_web_3.py
+++ python_web_3.py
@@ -7,19 +7,25 @@ import time
 
 app = Flask(__name__)
 
+ganache_url = "http://127.0.0.1:7545" #url of local blockchain network
+web3 = Web3(Web3.HTTPProvider(ganache_url))
+web3.eth.defaultAccount = web3.eth.accounts[0]
+address = web3.toChecksumAddress("0x7FE47FE0CF25433F7A1B8CCCb1b63D822eC35141") #Will convert an upper or lowercase Ethereum address to a checksum address
+abi = json.loads('[{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"string","name":"_fruitName","type":"string"},{"internalType":"uint256","name":"_temperature","type":"uint256"},{"internalType":"uint256","name":"_weight","type":"uint256"},{"internalType":"uint256","name":"_noOfDays","type":"uint256"},{"internalType":"bool","name":"_flag","type":"bool"}],"name":"addFruit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"fruitArray","outputs":[{"internalType":"string","name":"fruitName","type":"string"},{"internalType":"uint256","name":"temperature","type":"uint256"},{"internalType":"uint256","name":"weight","type":"uint256"},{"internalType":"uint256","name":"noOfDays","type":"uint256"},{"internalType":"bool","name":"flag","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"}],"name":"output","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"uint256","name":"_expiryDays","type":"uint256"}],"name":"threshold_value_checking","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
+contract = web3.eth.contract(address = address, abi = abi)
+
 def op(idValue, fruit, temperature, weight, noOfDays, flag):
-    ganache_url = "http://127.0.0.1:7545" #url of local blockchain network
-    web3 = Web3(Web3.HTTPProvider(ganache_url))
-    web3.eth.defaultAccount = web3.eth.accounts[0]
-    address = web3.toChecksumAddress("0xBd958A56c87A43AE34c4D0adBa2826F7F897Dccd") #Will convert an upper or lowercase Ethereum address to a checksum address
-    abi = json.loads('[{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"string","name":"_fruitName","type":"string"},{"internalType":"uint256","name":"_temperature","type":"uint256"},{"internalType":"uint256","name":"_weight","type":"uint256"},{"internalType":"uint256","name":"_noOfDays","type":"uint256"},{"internalType":"bool","name":"_flag","type":"bool"}],"name":"addFruit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"fruitArray","outputs":[{"internalType":"string","name":"fruitName","type":"string"},{"internalType":"uint256","name":"temperature","type":"uint256"},{"internalType":"uint256","name":"weight","type":"uint256"},{"internalType":"uint256","name":"noOfDays","type":"uint256"},{"internalType":"bool","name":"flag","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"}],"name":"output","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"}],"name":"threshold_value_checking","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
-    contract = web3.eth.contract(address = address, abi = abi)
     dateOfCultivated = date.today()
     tx_hash = contract.functions.addFruit(idValue, fruit, temperature, weight, noOfDays, flag).transact()
     web3.eth.waitForTransactionReceipt(tx_hash) #Wait for the transaction to be mined, and get the transaction receipt
-    dateOfPurchasing = date(2021, 2, 24)
-    #noOfDaysItHasBeen =  dateOfPurchasing-dateOfCultivated
-    contract.functions.threshold_value_checking(idValue).transact() #contract.functions.threshold_value_checking(idValue, noOfDaysItHasBeen.days).transact()
+    contract.functions.threshold_value_checking(idValue, 0).transact() 
+    return contract.functions.output(idValue).call()
+
+def op1(idValue):    
+    dateOfPurchasing = date(2021, 2, 24) #date.today()
+    dateOfCultivated = date.today()
+    noOfDaysItHasBeen =  dateOfPurchasing-dateOfCultivated
+    contract.functions.threshold_value_checking(idValue, noOfDaysItHasBeen.days).transact() #contract.functions.threshold_value_checking(idValue, noOfDaysItHasBeen.days).transact()
     return contract.functions.output(idValue).call()
 
 
@@ -44,6 +50,17 @@ def my_form_post():
     }
     result = {str(key): value for key, value in result.items()}
     return jsonify(result=result)
+
+@app.route('/join1', methods=['GET','POST'])
+def my_output():
+    text8 = request.form['text8']
+
+    out = op1(int(text8))
+    result1 = {
+        "output": out
+    }
+    result1 = {str(key): value for key, value in result1.items()}
+    return jsonify(result1=result1)
 
 if __name__ == '__main__':
     app.run(debug=True)
